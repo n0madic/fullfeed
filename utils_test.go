1cef765b060b46a73fdb8c599d318dc2bfd1ab99
Alexander Sheiko <alexander.sheiko@mgid.com>
2019-08-21 16:09:47 +0300

    Add utils tests

---
+++ utils_test.go
@@ -0,0 +1,37 @@
+package fullfeed
+
+import (
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func TestMakeAllLinksAbsolute(t *testing.T) {
+	html := `<a href="index.html"><img src="image.png" data-src="image.png"/></a>`
+	baseURL, _ := url.Parse("http://www.example.com")
+	attributes := []string{"src", "data-src", "href"}
+
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(html))
+	makeAllLinksAbsolute(baseURL, doc)
+	doc.Find("a,img").Each(func(i int, sel *goquery.Selection) {
+		for _, attr := range attributes {
+			if val, exists := sel.Attr(attr); exists {
+				if !strings.HasPrefix(val, "http://www.example.com") {
+					t.Errorf("Expected find absolute URL in %s=\"%s\"", attr, val)
+				}
+			}
+		}
+	})
+}
+
+func TestStringIsFiltered(t *testing.T) {
+	if !stringIsFiltered("test", []string{"test"}) {
+		t.Error("Expected find 'test' in ['test']")
+	}
+
+	if stringIsFiltered("test", []string{"test2"}) {
+		t.Error("Not expected to find 'test' in ['test2']")
+	}
+}
